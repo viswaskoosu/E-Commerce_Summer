f21a62d5fa75a3eef0884a02f1286a9f539e75ee
Koosu Viswas <210030016@iitdh.ac.in>
2024-05-25 02:32:05 +0530

    m

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ import OrderHistory from './Pages/OrderHistory';
 import ProductDetail from './Pages/ProductDetail';
 import AccountPage from './Pages/Account';
 import ContactInfo from './Pages/ContactInfo';
-import PaymentMethods from './Pages/PaymentMethods'; // Import the new page component
+import PaymentMethods from './Pages/PaymentMethods';
 import Products from './data';
+
 function App() {
   console.log('Window width: ' + window.innerWidth + 'px');
   const [, dispatch] = useStateValue();
@@ -33,25 +34,27 @@ function App() {
       <Router>
         <div className="app">
           <Header />
-          <Switch>
-            {/* Public Routes */}
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/favourites" component={FavoritesPage} />
-            <Route path="/orderhistory" component={OrderHistory} />
-            <Route path="/product/:id" component={ProductDetail} />
-            <Route exact path="/" component={Home} />
+          <div className="main-content">
+            <Switch>
+              {/* Public Routes */}
+              <Route path="/checkout" component={Checkout} />
+              <Route path="/favourites" component={FavoritesPage} />
+              <Route path="/orderhistory" component={OrderHistory} />
+              <Route path="/product/:id" component={ProductDetail} />
+              <Route exact path="/" component={Home} />
 
-            {/* Restricted Routes */}
-            <Route path="/signin" component={SignIn} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/forgotpassword" component={ForgotPassword} />
+              {/* Restricted Routes */}
+              <Route path="/signin" component={SignIn} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/forgotpassword" component={ForgotPassword} />
 
-            {/* Protected Routes */}
-            <Route path="/account" component={AccountPage} />
-            <Route path="/contactinfo" component={ContactInfo} />
-            <Route path="/paymentmethods" component={PaymentMethods} /> {/* Route for PaymentMethodsPage */}
-          </Switch>
-          <Footer />
+              {/* Protected Routes */}
+              <Route path="/account" component={AccountPage} />
+              <Route path="/contactinfo" component={ContactInfo} />
+              <Route path="/paymentmethods" component={PaymentMethods} />
+            </Switch>
+          </div>
+          <Footer className="footer" />
         </div>
       </Router>
     </StateProvider>
