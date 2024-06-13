11c304d86585cda27a48ef9fcd9c5edd57917914
Koosu Viswas <210030016@iitdh.ac.in>
2024-06-14 01:28:18 +0530

    address

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,38 +1,38 @@
 import React from "react";
 import "./Footer.css";
-
+import logo from '../Header/companyLogo.png'
 function Footer() {
   return (
     <>
       <div className="footer">
-        <div className="disclaimer-area">
+        {/* <div className="disclaimer-area">
           <p className="disclaimer-desc">
             <b>The best E-Commerce site for Construction</b>
           </p>
-        </div>
+        </div> */}
       </div>
       <div className="extra-data">
         <div className="link-section">
           <div className="first-row">
             <p className="bold">Get to Know Us</p>
             <p>Make Money with Us</p>
-            <p>Amazon Payment</p>
+            <p>IIT Dharwad Payment</p>
             <p>Let Us Help You</p>
           </div>
           <div className="second-row">
-            <p className="bold">About Amazon</p>
-            <p>Sell products on Amazon</p>
-            <p>Amazon Business Card</p>
-            <p>Amazon and COVID-19</p>
+            <p className="bold">About IIT Dharwad</p>
+            <p>Sell products on IIT Dharwad</p>
+            <p>IIT Dharwad Business Card</p>
+            <p>IIT Dharwad and COVID-19</p>
           </div>
           <div className="third-row">
             <p className="bold">Connect with Us</p>
-            <p>Sell apps on Amazon</p>
+            <p>Sell apps on IIT Dharwad</p>
             <p>Shop with Points</p>
             <p>Shipping Rates & Policies</p>
           </div>
           <div className="fourth-row">
-            <p className="bold">Amazon Cares</p>
+            <p className="bold">IIT Dharwad Cares</p>
             <p>Become an Affiliate</p>
             <p>Reload Your Balance</p>
             <p>Returns & Replacements</p>
@@ -43,25 +43,25 @@ function Footer() {
             <div className="first-row">
               <p className="bold">Get to Know Us</p>
               <p>Make Money with Us</p>
-              <p>Amazon Payment</p>
+              <p>IIT Dharwad Payment</p>
               <p>Let Us Help You</p>
             </div>
             <div className="second-row">
-              <p className="bold">About Amazon</p>
-              <p>Sell products on Amazon</p>
-              <p>Amazon Business Card</p>
-              <p>Amazon and COVID-19</p>
+              <p className="bold">About IIT Dharwad</p>
+              <p>Sell products on IIT Dharwad</p>
+              <p>IIT Dharwad Business Card</p>
+              <p>IIT Dharwad and COVID-19</p>
             </div>
           </div>
           <div className="second-one">
             <div className="third-row">
               <p className="bold">Connect with Us</p>
-              <p>Sell apps on Amazon</p>
+              <p>Sell apps on IIT Dharwad</p>
               <p>Shop with Points</p>
               <p>Shipping Rates & Policies</p>
             </div>
             <div className="fourth-row">
-              <p className="bold">Amazon Cares</p>
+              <p className="bold">IIT Dharwad Cares</p>
               <p>Become an Affiliate</p>
               <p>Reload Your Balance</p>
               <p>Returns & Replacements</p>
@@ -69,7 +69,7 @@ function Footer() {
           </div>
         </div>
         <div className="developer">
-          <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" className="amazon-img" />
+          <img src={logo} className="logo-img" />
           <div className="dev-data">
             <p>&copy; 2023 | Developed by </p>
             <a
